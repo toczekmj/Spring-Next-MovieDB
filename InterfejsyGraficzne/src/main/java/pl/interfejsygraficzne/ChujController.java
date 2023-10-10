cc9d22634bd1721cdbf42657e1e042a6d0f6e56e
michalharasim <mharasim74@gmail.com>
2023-10-10 18:46:10 +0200

    palestine

--- InterfejsyGraficzne/src/main/java/pl/interfejsygraficzne/ChujController.java
+++ InterfejsyGraficzne/src/main/java/pl/interfejsygraficzne/ChujController.java
@@ -4,10 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ChujController {
     @GetMapping("kuba")
     public String Chuj(){
         return "8=====D";
+    }
+
+    @GetMapping("palestine")
+    public List<String> palestine() {
+        return List.of("free", "number one");
     }
 }
