1af31a4ca2906dae0399c9d8da6089c947a8ee06
Michał Toczek <toczekmj@gmail.com>
2023-10-11 15:58:03 +0200

    poprawka dla szefa tak o

--- InterfejsyGraficzne/src/main/java/pl/interfejsygraficzne/Repository/IUserRepository.java
+++ InterfejsyGraficzne/src/main/java/pl/interfejsygraficzne/Repository/IUserRepository.java
@@ -1,10 +1,12 @@
 package pl.interfejsygraficzne.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.interfejsygraficzne.Model.User;
 
 import java.util.List;
 
+@Repository
 public interface IUserRepository extends JpaRepository<User, Integer> {
     List<User> findByFirstName(String name);
 }
