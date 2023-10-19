5cea0250cb67d6517241ffb822165a9605408d74
Michał Toczek <toczekmj@gmail.com>
2023-10-19 14:11:24 +0200

    change error handling to redirect to swagger automatically

--- InterfejsyGraficzne/src/main/java/pl/interfejsygraficzne/controller/MyErrorController.java
+++ InterfejsyGraficzne/src/main/java/pl/interfejsygraficzne/controller/MyErrorController.java
@@ -1,5 +1,6 @@
 package pl.interfejsygraficzne.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyErrorController implements ErrorController {
     @RequestMapping("/error")
-    public String Error(){
-        return """
-                Pewnie szukasz tego xd
-                <a href="http://api.projektimdb.it/swagger-ui/index.html">SwaggerUI</a>""";
+    public void Error(HttpServletResponse response){
+        response.setHeader("Location", "http://api.projektimdb.it/swagger-ui/index.html");
+        response.setStatus(302);
+//        return """
+//                Pewnie szukasz tego xd
+//                <a href="http://api.projektimdb.it/swagger-ui/index.html">SwaggerUI</a>""";
     }
 }
