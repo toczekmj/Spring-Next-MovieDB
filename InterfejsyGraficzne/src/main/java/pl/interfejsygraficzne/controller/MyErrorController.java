8cf4a597a1282dfe07c53ac1c7f1a4c5a9a2ea19
Michał Toczek <toczekmj@gmail.com>
2023-10-19 14:41:13 +0200

    revert changes

--- InterfejsyGraficzne/src/main/java/pl/interfejsygraficzne/controller/MyErrorController.java
+++ InterfejsyGraficzne/src/main/java/pl/interfejsygraficzne/controller/MyErrorController.java
@@ -9,11 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyErrorController implements ErrorController {
     @RequestMapping("/error")
     public void Error(HttpServletResponse response){
-        response.setHeader("Location", "https://google.com");
-//        response.setHeader("Location", "http://api.projektimdb.it/swagger-ui/index.html");
+        response.setHeader("Location", "http://api.projektimdb.it/swagger-ui/index.html");
         response.setStatus(302);
-//        return """
-//                Pewnie szukasz tego xd
-//                <a href="http://api.projektimdb.it/swagger-ui/index.html">SwaggerUI</a>""";
     }
 }
