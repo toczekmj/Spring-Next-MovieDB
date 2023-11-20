12a04293c5e630f920297f6890bb24996a92e492
Michał Toczek <toczekmj@gmail.com>
2023-11-20 12:50:00 +0100

    updated dbconfiguration

--- InterfejsyGraficzne/src/main/java/pl/interfejsygraficzne/Beans/DBConfiguration.java
+++ InterfejsyGraficzne/src/main/java/pl/interfejsygraficzne/Beans/DBConfiguration.java
@@ -4,14 +4,13 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import pl.interfejsygraficzne.Misc.TerminalColours;
 
 import javax.sql.DataSource;
 import java.util.Objects;
 
 @Configuration
 public class DBConfiguration {
-
-
     private final Environment env;
 
     public DBConfiguration(Environment env){
@@ -20,17 +19,35 @@ public class DBConfiguration {
     @Bean
     public DataSource CreadeDbConfiguration(){
         DataSourceBuilder<?> dsb = DataSourceBuilder.create();
-        if(Objects.requireNonNull(env.getProperty("working.environment")).equalsIgnoreCase("dev"))
-            dsb.url("jdbc:mysql://maluch.mikr.us:40213/InterfejsyTest");
-        else if(Objects.requireNonNull(env.getProperty("working.environment")).equalsIgnoreCase("prod"))
-            dsb.url("jdbc:mysql://maluch.mikr.us:40213/Interfejsy");
+        String environment = env.getProperty("working.environment");
 
-        String password = env.getProperty("database.password");
-        dsb.password(password);
+        if(environment == null || environment.isEmpty())
+        {
+            System.out.println(TerminalColours.ANSI_RED_BACKGROUND + "Please set environment in application.properties" + TerminalColours.ANSI_RESET);
+        }
+        else {
+            //set environment
+            if(environment.equalsIgnoreCase("dev"))
+                dsb.url("jdbc:mysql://maluch.mikr.us:40213/InterfejsyTest");
+            else if(Objects.requireNonNull(env.getProperty("working.environment")).equalsIgnoreCase("prod"))
+                dsb.url("jdbc:mysql://maluch.mikr.us:40213/Interfejsy");
+            else
+                System.out.println(TerminalColours.ANSI_RED + "Not a valid environment in application.properties" + TerminalColours.ANSI_RESET);
 
-        String login = env.getProperty("database.username");
-        dsb.username(login);
+            //set password
+            String password = env.getProperty("database.password");
+            if(password != null && !password.isEmpty())
+                dsb.password(password);
+            else
+                System.out.println(TerminalColours.ANSI_RED + "Please set password in application.properties" + TerminalColours.ANSI_RESET);
 
+            //set login
+            String login = env.getProperty("database.username");
+            if(login != null && !login.isEmpty())
+                dsb.username(login);
+            else
+                System.out.println(TerminalColours.ANSI_RED + "Please set username in application.properties" + TerminalColours.ANSI_RESET);
+        }
         return dsb.build();
     }
 }
