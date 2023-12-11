016ca5cd09b67b6f96911eddcb36df472bec94a0
Michał Toczek <toczekmj@gmail.com>
2023-12-11 21:25:17 +0100

    rating i inne pierdoly

--- frontend/src/features/movies/movies.tsx
+++ frontend/src/features/movies/movies.tsx
@@ -9,37 +9,37 @@ interface Movies {
 
 export const MOVIES: Array<Movies> = [
   {
-    title: "Movie1",
+    title: "8",
     popularity: 12,
     addDate: "2021-05-12",
   },
   {
-    title: "Movie2",
+    title: "2",
     popularity: 13,
     addDate: "2021-05-12",
   },
   {
-    title: "Movie3",
+    title: "3",
     popularity: 14,
     addDate: "2021-05-12",
   },
   {
-    title: "Movie5",
+    title: "4",
     popularity: 15,
     addDate: "2021-05-12",
   },
   {
-    title: "Movie4",
+    title: "5",
     popularity: 17,
     addDate: "2021-05-12",
   },
   {
-    title: "Movie6",
+    title: "6",
     popularity: 18,
     addDate: "2021-04-12",
   },
   {
-    title: "Movie10",
+    title: "7",
     popularity: 54,
     addDate: "2021-03-12",
   },
@@ -49,7 +49,7 @@ export const MOVIES: Array<Movies> = [
     addDate: "2021-02-12",
   },
   {
-    title: "Movie12",
+    title: "9",
     popularity: 44,
     addDate: "2021-01-12",
   },
--- frontend/src/pages/list/[id].tsx
+++ frontend/src/pages/list/[id].tsx
@@ -1,79 +1,111 @@
-
 import {
-  Card,
-  CardBody,
-  CardHeader,
-  Stack,
-  Text,
+    Card,
+    CardBody,
+    CardHeader, List, ListItem,
+    Stack,
+    Text,
 } from "@chakra-ui/react";
+import ReactStars from "react-rating-stars-component"
 import React from "react";
 import useSWR from "swr";
-import {func} from "prop-types";
+import {useRouter} from "next/router";
+import {render} from "react-dom";
 
 const fetcher = (...args: any[]) => fetch(...args).then((res) => res.json())
 
 const fetchActors = function (actorsList: any) {
-  const aList = [];
-  for(let i = 0; i < actorsList.length; i++){
-    aList.push(actorsList[i].firstName + " " + actorsList[i].lastName);
-    if(i != actorsList.length-1){
-      aList[i] = aList[i] + ',';
+    const aList = [];
+    for (let i = 0; i < actorsList.length; i++) {
+        aList.push(actorsList[i].firstName + " " + actorsList[i].lastName);
+        if (i != actorsList.length - 1) {
+            aList[i] = aList[i] + ',';
+        }
     }
-  }
-  return aList;
+    return aList;
 }
+
+const fetchComments = function(commentList: any) {
+    let comments = "";
+    for(let i = 0; i < commentList.length; i++){
+        comments +=  "<b>Użytkownik " + commentList[i].id + "</b>: " + commentList[i].text + "<br>";
+    }
+    return comments;
+}
+
+const fetchRating = function(rating: any){
+
+
+
+    const votes: number = parseInt(rating.votesCount);
+    const plot: number = parseInt(rating.plot);
+    const acting: number = parseInt(rating.acting);
+    const scenography: number = parseInt(rating.scenography)
+
+    return [plot/votes, acting/votes, scenography/votes];
+
+
+
+}
+
 
 export default function SingleMoviePage() {
-  const {data, error} = useSWR('http://localhost/api/v1/movies/get/byid/1', fetcher);
-  if(error) return <div>Failed to fetch</div>
-  if(!data) return <div>Loading</div>
-  const actorsList = fetchActors(data.actors);
 
-  return (
-    <Stack h="100vh" align="center" justifyContent="center" spacing={55}>
-      <Card w="40%">
-        <CardHeader>
-          <Text fontSize="26px" fontWeight="700">
-            {data?.title}
-          </Text>
-        </CardHeader>
-        <CardBody>
-          {/* trzeba dopisac jakies rzeczy w stylu reżyser, aktorzy itd  */}
-          {/* <Text>Dodano: {movie?.addDate}</Text> */}
-          {/* <Text>{movie?.popularity}</Text> */}
-          <Text>Reżyser: {data.director}</Text>
-          <Text>Rok produkcji: {data.productionYear}</Text>
-          <Text>Główna obsada: {actorsList}</Text>
-          
-        </CardBody>
-      </Card>
+    const router = useRouter();
+    const {id} = router.query;
+    const APIURL = "http://localhost/api/v1/movies/get/byid/" + id?.toString();
 
-      <Card w="20%">
-        <CardHeader>
-          <Text fontSize="26px" fontWeight="700">
-            Komentarze
-          </Text>
-        </CardHeader>
-        <CardBody>
-          <Text></Text>
-          <Text>Użytkownik1: Bardzo dobry film</Text>
-          <Text>
-            Użytkownik2: Muszę powiedzieć, że pomimo nie lubię tego reżysera,
-            ten film zaskoczył mnie pozytywnie.
-          </Text>
-        </CardBody>
-      </Card>
-    </Stack>
-  );
+
+    // const {data, error} = useSWR('http://api.projektimdb.it/api/v1/movies/get/byid/1', fetcher);
+    const {data, error} = useSWR(APIURL, fetcher);
+    if (error) return <div>Failed to fetch</div>
+    if (!data) return <div>Loading</div>
+    const actorsList = fetchActors(data.actors);
+    const commentList = fetchComments(data.comments);
+    const rating = fetchRating(data.rating); //plot, acting, scenography
+
+    console.log("plot: " + rating[0] + ", acting: " + rating[1] + ", scenography: " + rating[2]);
+
+    return (
+        <Stack h="100vh" align="center" justifyContent="center" spacing={55}>
+            <Card w="40%">
+                <CardHeader>
+                    <Text fontSize="26px" fontWeight="700">
+                        {data?.title}
+                    </Text>
+                </CardHeader>
+
+                <CardBody>
+                    <Text><b>Reżyser:</b> {data.director}</Text>
+                    <Text><b>Rok produkcji:</b> {data.productionYear}</Text>
+                    <Text><b>Główna obsada:</b> {actorsList}</Text>
+                    <Text><b>Opinie telewidzów:</b></Text>
+                    <Text>
+                        Aktorstwo:
+                        <ReactStars count={5} value={rating[1]} size={20} isHalf={true} edit={false}></ReactStars>
+                    </Text>
+                    <Text>
+                        Fabuła:
+                        <ReactStars count={5} value={rating[0]} size={20} isHalf={true} edit={false}></ReactStars>
+                    </Text>
+                    <Text>
+                        Scenografia:
+                        <ReactStars count={5} value={rating[2]} size={20} isHalf={true} edit={false}></ReactStars>
+                    </Text>
+                </CardBody>
+            </Card>
+
+
+            {/*komentarze */}
+            <Card w="40%">
+                <CardHeader>
+                    <Text fontSize="26px" fontWeight="700">
+                        Komentarze
+                    </Text>
+                </CardHeader>
+                <CardBody>
+                    <Text dangerouslySetInnerHTML={{__html: commentList}}></Text>
+                </CardBody>
+            </Card>
+        </Stack>
+    );
 }
-
-
-const callAPI = async () => {
-	try {
-		const res = await fetch('http://localhost/api/v1/movies/get/byid/1');
-		const data = await res.json();
-		console.log(data);
-	} catch (err) {
-		console.log(err);
-	}
-};
